5ec481118bc6fcc501c640aa1f2bd4bf65030165
Igor Josiel <igorjosielvitalino@gmail.com>
2026-01-04 23:38:07 -0300

    Cria novo componente para renderizar o card do próximo dia da semana.

--- src/components/ForecastItem/index.jsx
+++ src/components/ForecastItem/index.jsx
@@ -1,10 +1,26 @@
-/* import "../styles.css"; */
+import "./styles.css";
+
+const getWeekday = (dateString) => {
+  const [day, month] = dateString.split("/");
+
+  const dateObj = new Date(new Date().getFullYear(), month - 1, day);
+  const weekday = Intl.DateTimeFormat("pt-BR", { weekday: "long" }).format(
+    dateObj
+  );
+
+  return weekday.charAt(0).toUpperCase() + weekday.split("-")[0].slice(1);
+};
 
 const ForecastItem = ({ date, min, max, condition, description }) => {
   return (
     <div className="forecast-item">
-      <p className="forecast-day">({date})</p>
+      <div className="forecast-date">
+        <p>{getWeekday(date)}</p>
+        <p>({date})</p>
+      </div>
+
       <img src={`./icons-weather/${condition}.svg`} alt={description} />
+
       <p className="forecast-temp">
         {min}/{max}°
       </p>
